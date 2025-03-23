20b99fd6d5683d050d4c9cbb6cf07f529bd99516
PabloVallecillos <vallecillospablo@gmail.com>
2025-03-23 10:26:25 +0100

    fix(test): Correct syntax for assertStatus in ApiDocsTest

--- back/tests/Feature/ApiDocsTest.php
+++ back/tests/Feature/ApiDocsTest.php
@@ -6,5 +6,5 @@ it('returns a valid JSON with openapi 3.1.0', function () {
     $response = getJson('/docs/api.json');
 
     // TODO: change when authentication is implemented
-    $response->assertStatus(status:403);
+    $response->assertStatus(status: 403);
 });
