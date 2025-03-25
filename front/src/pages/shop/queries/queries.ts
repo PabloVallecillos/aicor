52bcbbf00f6d9f4ff89e8450c670f9af61a4d058
PabloVallecillos <vallecillospablo@gmail.com>
2025-03-25 12:37:43 +0100

    fix(products): Implement product images, and fixed infinite scroll

--- front/src/pages/shop/queries/queries.ts
+++ front/src/pages/shop/queries/queries.ts
@@ -1,11 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
-import type { RequestListEndpoint, ApiListResponse } from '@/types/api';
-import { getProducts } from '@/lib/api';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import type { ApiListResponse } from '@/types/api';
 import type { Product } from '@/types';
+import { getProducts } from '@/lib/api';
 
-export const useProducts = (params: RequestListEndpoint) => {
-  return useQuery<ApiListResponse<Product>>({
-    queryKey: ['products', params],
-    queryFn: async () => getProducts(params)
+export const useGetInfiniteProducts = () => {
+  return useInfiniteQuery<ApiListResponse<Product>>({
+    queryKey: ['products'],
+    queryFn: async ({ pageParam = 1 }) => {
+      return await getProducts({ page: pageParam, per_page: 24 });
+    },
+    initialPageParam: 1,
+    getNextPageParam: (data: ApiListResponse<Product>) => {
+      if (data.current_page < data.last_page) {
+        return data.current_page + 1;
+      }
+      return undefined;
+    },
+    retry: false,
   });
 };
