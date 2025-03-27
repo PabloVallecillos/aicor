f32c83c6e99c7b2e367f8307b65846d45e13d5f2
PabloVallecillos <vallecillospablo@gmail.com>
2025-03-27 21:50:48 +0100

    feat: enable session management and CORS support for API requests

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -13,7 +13,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
-  }
+  },
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
